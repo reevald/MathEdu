5f70a157fedbb070f3672c58a337d938bba4b34e
Mochammad Galang Rivaldo <mochammadgalangrivaldo@gmail.com>
2021-08-18 11:21:57 +0700

    Fix base_url

--- public/index.php
+++ public/index.php
@@ -1,6 +1,6 @@
 <?php
 // IMPORTANT
-$env_dev = 'development';
+$env_dev = 'production';
 
 function get_url($addon) {
 	if($env_dev == 'production'){
@@ -16,11 +16,11 @@ function get_url($addon) {
 <html>
 <head>
 	<meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Test Deploy</title>
-    <meta name="description" content="Aplikasi Belajar Bangun Datar melalui Game Tebak Gambar Berbasis Artificial Intelligence">
-    <link rel="stylesheet" href="<?=get_url('css/app-output.css')?>">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Test Deploy</title>
+  <meta name="description" content="Aplikasi Belajar Bangun Datar melalui Game Tebak Gambar Berbasis Artificial Intelligence">
+  <link rel="stylesheet" href="<?=get_url('css/app-output.css')?>">
 </head>
 <body>
 	<h1 class="text-purple-700"><?php echo "Hello World : " ?><?= get_url('')?></h1>
