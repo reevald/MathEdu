5d02c16cb627519b301bc9b10810866283360c52
Mochammad Galang Rivaldo <mochammadgalangrivaldo@gmail.com>
2021-08-18 10:50:59 +0700

    Config tailwind.css

--- public/index.php
+++ public/index.php
@@ -1,9 +1,28 @@
+<?php
+function get_url($addon) {
+	$pageURL = 'http';
+	if ($_SERVER["HTTPS"] == "on") {$pageURL .= "s";}
+	$pageURL .= "://";
+	if ($_SERVER["SERVER_PORT"] != "80") {
+ 		$pageURL .= $_SERVER["SERVER_NAME"].":".$_SERVER["SERVER_PORT"].$_SERVER["REQUEST_URI"];
+ 	} else {
+  	$pageURL .= $_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"];
+	}
+	$pageURL .= $addon;
+	return $pageURL;
+}
+?>
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Test Deploy</title>
+	<meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Test Deploy</title>
+    <meta name="description" content="Aplikasi Belajar Bangun Datar melalui Game Tebak Gambar Berbasis Artificial Intelligence">
+    <link rel="stylesheet" href="<?=get_url('css/app-output.css')?>">
 </head>
 <body>
-	<h1><?php echo "Hello World" ?></h1>
+	<h1 class="text-purple-700"><?php echo "Hello World" ?></h1>
 </body>
 </html>
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ['public/*.php'],
+  darkMode: 'class', // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
