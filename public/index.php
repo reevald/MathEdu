f5b0be6fa78804c9e766fb646aa8638fca3f6359
Mochammad Galang Rivaldo <mochammadgalangrivaldo@gmail.com>
2021-08-18 11:38:52 +0700

    Fix base_url

--- public/index.php
+++ public/index.php
@@ -1,21 +1,19 @@
 <?php
-// IMPORTANT
-$env_dev = 'production';
-
 function get_url($addon) {
-	if($env_dev == 'production'){
-		$base_url = 'https://mathedu-ariga.herokuapp.com/';
-	}else{
-		$base_url = 'http://localhost/MathEdu/public/';
-	}
-	$base_url .= $addon;
-	return $base_url;
+  $production = True;
+  if($production){
+    $base_url = 'https://mathedu-ariga.herokuapp.com/';
+  }else{
+    $base_url = 'http://localhost/MathEdu/public/';
+  }
+  $base_url .= $addon;
+  return $base_url;
 }
 ?>
 <!DOCTYPE html>
 <html>
 <head>
-	<meta charset="UTF-8">
+  <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Test Deploy</title>
@@ -23,6 +21,6 @@ function get_url($addon) {
   <link rel="stylesheet" href="<?=get_url('css/app-output.css')?>">
 </head>
 <body>
-	<h1 class="text-purple-700"><?php echo "Hello World : " ?><?= get_url('')?></h1>
+  <h1 class="text-purple-700"><?php echo "Hello World : " ?><?= get_url('')?></h1>
 </body>
 </html>
