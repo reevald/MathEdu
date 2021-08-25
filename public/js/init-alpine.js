3b80a7ae6b2bd481b2f10aed65157a9cd6345c0d
Mochammad Galang Rivaldo <mochammadgalangrivaldo@gmail.com>
2021-08-25 19:42:52 +0700

    Create modal handler with alpinejs

--- public/js/init-alpine.js
+++ public/js/init-alpine.js
@@ -1,1 +1,28 @@
-// Under development
+function htmlData(){
+  return {
+    // Menu handler
+    is_menu_open: false,
+    toggleMenu(){
+      this.is_menu_open = !this.is_menu_open;
+    },
+    closeMenu(){
+      this.is_menu_open = false;
+    },
+    // Size handler
+    is_size_pen_open: false,
+    toggleSizePen(){
+      this.is_size_pen_open = !this.is_size_pen_open;
+    },
+    closeSizePen(){
+      this.is_size_pen_open = false;
+    },
+    // Color handler
+    is_color_pen_open: false,
+    toggleColorPen(){
+      this.is_color_pen_open = !this.is_color_pen_open;
+    },
+    closeColorPen(){
+      this.is_color_pen_open = false;
+    }
+  }
+}
